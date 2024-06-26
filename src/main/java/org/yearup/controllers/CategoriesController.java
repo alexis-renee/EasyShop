753bd29498d1658c03eeb50d2ab0d262e9c76733
alexis-renee <agrisom@pgh.yearup.org>
2024-06-26 16:34:12 -0400

    Commit after merge

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,8 +1,10 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -13,14 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("categories")
 @CrossOrigin
-
-// add the annotations to make this a REST controller
-// add the annotation to make this controller the endpoint for the following url
-    // http://localhost:8080/categories
-// add annotation to allow cross site origin requests
-
-public class CategoriesController
-{
+public class CategoriesController {
     private CategoryDao categoryDao;
     private ProductDao productDao;
 
@@ -33,58 +28,103 @@ public class CategoriesController
     // add the appropriate annotation for a get action
     @GetMapping("")
     @PreAuthorize("permitAll()")
-    public List<Category>getAll()
-
-    {
-        // find and return all categories
-      return categoryDao.getAllCategories();
+    public List<Category> getAll() {
+        try {
+            return categoryDao.getAllCategories();
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
     }
+// find and return all categories
+
 
     // add the appropriate annotation for a get action
     @GetMapping("{id}")
-    public Category getById(@PathVariable int id)
+    public Category getById(@PathVariable int id) {
+        Category category = null;
+        try {
+            category = categoryDao.getById(id);
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
 
-    {
+        if (category == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return category;
+
         // get the category by id
-        return null;
+
+
     }
 
     // the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
     @GetMapping("{categoryId}/products")
-    public List<Product> getProductsById(@PathVariable int categoryId)
-    {
+    public List<Product> getProductsById(@PathVariable int categoryId) {
+        try {
+            var product = productDao.getById(categoryId);
+
+            if (product == null)
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+            return (List<Product>) product;
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
         // get a list of product by categoryId
-        return null;
+        // list all product by category id
+        // return productDAO.method
     }
 
     // add annotation to call this method for a POST action
     // add annotation to ensure that only an ADMIN can call this function
     @PreAuthorize("ADMIN")
     @PostMapping
-    public Category addCategory(@RequestBody Category category)
-    {
+    public Category addCategory(@RequestBody Category category) {
+        try {
+            return categoryDao.create(category);
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
         // insert the category
-        return null;
+        // add response status
+
     }
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
-    @PreAuthorize("ADMIN")
+    @PreAuthorize("has Role(ADMIN")
     @PutMapping("{id}")
-    public void updateCategory(@PathVariable int id, @RequestBody Category category)
-    {
+    public void updateCategory(@PathVariable int id, @RequestBody Category category) {
+        try {
+            categoryDao.update(id, category);
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
         // update the category by id
+
     }
 
 
     // add annotation to call this method for a DELETE action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
-    @PreAuthorize("ADMIN")
+    @PreAuthorize("has Role(ADMIN")
     @DeleteMapping("{id}")
-    public void deleteCategory(@PathVariable int id)
+    public void deleteCategory(@PathVariable int id) {
+        try {
+            var category = categoryDao.getById(id);
 
-    {
-        // delete the category by id
+            if (category == null)
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+            categoryDao.delete(id);
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
     }
+    // delete the category by id
+    // add response status
+
 }
+
--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -10,6 +10,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -21,18 +22,32 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
 
     @Override
     public List<Category> getAllCategories() {
+        List<Category> categories = new ArrayList<>();
 
+        String sql = "SELECT * FROM categories";
+
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet row = statement.executeQuery();
+
+            while (row.next()) {
+                Category category = mapRow(row);
+                categories.add(category);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return categories;
     }
-}
-
 
 
     @Override
-    public Category getById(int categoryId)
-    {
-        String sql = "SELECT * FROM products WHERE product_id = ?";
-        try (Connection connection = getConnection())
-        {
+    public Category getById(int categoryId) {
+        String sql = "SELECT * FROM products WHERE category_id = ?";
+        try (Connection connection = getConnection()) {
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.setInt(1, categoryId);
 
@@ -41,34 +56,23 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
             if (row.next()) {
                 return mapRow(row);
             }
-                catch (SQLException e) {
-                throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
 
         }
         return null;
     }
 
-    // get category by id
-        return null;
-    }
 
     @Override
-    public Category create(Category category)
-    {
-        String sql = "INSERT INTO products(name, price, category_id, description, color, image_url, stock, featured) " +
-                " VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+    public Category create(Category category) {
+        String sql = "INSERT INTO products(name, description) " +
+                " VALUES (?, ?);";
 
-        try (Connection connection = getConnection())
-        {
+        try (Connection connection = getConnection()) {
             PreparedStatement statement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
             statement.setString(1, category.getName());
-            statement.setBigDecimal(2, category.getPrice());
-            statement.setInt(3, category.getCategoryId());
-            statement.setString(4, category.getDescription());
-            statement.setString(5, category.getColor());
-            statement.setString(6, category.getImageUrl());
-            statement.setInt(7, category.getStock());
-            statement.setBoolean(8, category.isFeatured());
+            statement.setString(2, category.getDescription());
 
             int rowsAffected = statement.executeUpdate();
 
@@ -78,88 +82,64 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
 
                 if (generatedKeys.next()) {
                     // Retrieve the auto-incremented ID
-                    int orderId = generatedKeys.getInt(1);
+                    int categoryId = generatedKeys.getInt(1);
 
                     // get the newly inserted category
-                    return getById(orderId);
+                    return getById(categoryId);
                 }
             }
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return null;
     }
-        // create a new category
-        return null;
+    // create a new category
 
 
     @Override
-    public void update(int categoryId, Category category)
-    {
-        String sql = "UPDATE products" +
+    public void update(int categoryId, Category category) {
+        String sql = "UPDATE category" +
                 " SET name = ? " +
-                "   , price = ? " +
                 "   , category_id = ? " +
                 "   , description = ? " +
-                "   , color = ? " +
-                "   , image_url = ? " +
-                "   , stock = ? " +
-                "   , featured = ? " +
-                " WHERE product_id = ?;";
+                " WHERE category_id = ?;";
 
-        try (Connection connection = getConnection())
-        {
+        try (Connection connection = getConnection()) {
             PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setString(1, product.getName());
-            statement.setBigDecimal(2, product.getPrice());
-            statement.setInt(3, product.getCategoryId());
-            statement.setString(4, product.getDescription());
-            statement.setString(5, product.getColor());
-            statement.setString(6, product.getImageUrl());
-            statement.setInt(7, product.getStock());
-            statement.setBoolean(8, product.isFeatured());
-            statement.setInt(9, productId);
+            statement.setString(1, category.getName());
+            statement.setString(2, category.getDescription());
+            statement.setInt(3, categoryId);
 
             statement.executeUpdate();
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         // update category
     }
 
     @Override
-    public void delete(int categoryId)
-    {
+    public void delete(int categoryId) {
         String sql = "DELETE FROM products " +
                 " WHERE product_id = ?;";
 
-        try (Connection connection = getConnection())
-        {
+        try (Connection connection = getConnection()) {
             PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setInt(1, productId);
+            statement.setInt(1, categoryId);
 
             statement.executeUpdate();
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
-        // delete category
+    // delete category
 
-    private Category mapRow(ResultSet row) throws SQLException
-    {
+
+    private Category mapRow(ResultSet row) throws SQLException {
         int categoryId = row.getInt("category_id");
         String name = row.getString("name");
         String description = row.getString("description");
 
-        Category category = new Category()
-
-        {{
+        Category category = new Category() {{
             setCategoryId(categoryId);
             setName(name);
             setDescription(description);
@@ -167,5 +147,7 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
 
         return category;
     }
-
 }
+
+
+
